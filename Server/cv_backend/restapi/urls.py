93565e57ea683a0b06b7d0c40c5a26219165875b
Hristijan07 <hristijanvicoroski07@gmail.com>
2022-05-15 06:05:47 +0200

    new endpoints

--- Server/cv_backend/restapi/urls.py
+++ Server/cv_backend/restapi/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     path('test', views.test, name='test'),
     path('pdf', views.pdf_recieve, name='pdf_recieve'),
+    path('del_pdf', views.delete_pdfs, name='delete_pdfs'),
+    path('get_seniority', views.get_seniority, name='get_seniority'),
+    path('get_name', views.get_name, name='get_name'),
+    path('get_email', views.get_email, name='get_email'),
+    path('get_faculty', views.get_faculty, name='get_faculty'),
+    path('get_address', views.get_address, name='get_address'),
+    path('get_pr_lang', views.get_pr_lang, name='get_pr_lang'),
 ]
--- Server/cv_backend/restapi/views.py
+++ Server/cv_backend/restapi/views.py
@@ -6,7 +6,11 @@ import json
 import PyPDF2 as pdf
 import slate3k as slate
 import os
+import shutil
 
+
+cvs = []
+directory = "pdfs/"
 
 @api_view(['GET', 'POST', ])
 def test(request):
@@ -27,14 +31,10 @@ def pdf_recieve(request):
         f = request.data['file'].file
         f.seek(0)
         pdf = f.read()
-        #print(pdf)
 
         with open("pdfs/my_file.pdf", "wb") as binary_file:
             # Write bytes to file
             binary_file.write(pdf)
-
-        cvs = []
-        directory = "pdfs/"
 
         for filename in os.listdir(directory):
             f = os.path.join(directory, filename)
@@ -47,26 +47,108 @@ def pdf_recieve(request):
                     t2 = t2.replace("–", "-")
                     cvs.append(t2)
 
-        print(cvs[0])
-
-        # extracted_text = slate.PDF(pdf)
-        # t = extracted_text[0]
-        # print(t)
-        # with open(f, 'rb') as fopen:
-        #     q = fopen.read()
-        #     print(q.decode())
-
-        # with open(f.read().decode(), 'rb') as f:
-            
-        #     extracted_text = slate.PDF(f)
-        #     print(extracted_text)
-        # extracted_text = slate.PDF(f)
-        # t = extracted_text[0]
-        # t2 = t.replace("\n", " ")
-        # t2 = t2.replace("–", "-")
-        # print(extracted_text)
-
         d = {"test": 5}
-
         response = json.dumps(d)
         return Response(response, status=status.HTTP_200_OK)
+
+@api_view(['GET', 'POST', ])
+def delete_pdfs(request):
+    if request.method == 'GET':
+        shutil.rmtree('pdfs')
+        os.mkdir('pdfs')
+
+        return Response(status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def get_seniority(request):
+    if request.method == 'GET':
+        rez = seniority(cvs[0])
+
+        d = {"seniority": rez}
+        response = json.dumps(d)
+        return Response(response)
+
+@api_view(['GET'])
+def get_name(request):
+    if request.method == 'GET':
+        rez = name(cvs[0])
+
+        d = {"name": rez}
+        response = json.dumps(d)
+        return Response(response)
+
+@api_view(['GET'])
+def get_email(request):
+    if request.method == 'GET':
+        rez = email(cvs[0])
+
+        d = {"email": rez}
+        response = json.dumps(d)
+        return Response(response)
+
+@api_view(['GET'])
+def get_faculty(request):
+    if request.method == 'GET':
+        rez = faculty(cvs[0])
+
+        d = {"faculty": rez}
+        response = json.dumps(d)
+        return Response(response)
+
+@api_view(['GET'])
+def get_address(request):
+    if request.method == 'GET':
+        rez = address(cvs[0])
+        print(rez)
+
+        d = {"address": rez}
+        response = json.dumps(d)
+        return Response(response)
+
+@api_view(['GET'])
+def get_pr_lang(request):
+    if request.method == 'GET':
+        rez = pr_lang(cvs[0])
+
+        d = {"pr_lang": rez}
+        response = json.dumps(d)
+        return Response(response)
+
+
+
+# ---------------------------------- Helping functions ---------------------------------- #
+def godine_rada(date):
+    date1, date2 = date.split("-")
+    godina1 = date1.split("/")[1]
+    godina2 = date2.split("/")[1]
+    god = abs(int(godina1.strip()[-2:]) - int(godina2.strip()[-2:]))
+    return god
+
+def seniority(cv):
+    first_work = cv.split("  ")[7]
+    second_work = cv.split("  ")[18]
+    all_years = godine_rada(first_work) + godine_rada(second_work)
+    if (all_years < 3):
+        return "Junior"
+    elif (3 < all_years < 6):
+        return "Mid"
+    else:
+        return "Senior"
+
+def name(cv):
+    return cv.split("  ")[0]
+
+def email(cv):
+    a, mail = cv.split("  ")[3].strip().split(" ")
+    return mail
+
+def faculty(cv):
+    return cv.split("  ")[37] + " " + cv.split("  ")[38]
+
+def address(cv):
+    return cv.split("  ")[1].split(":")[1].strip()
+
+def pr_lang(cv):
+    l = cv.split("  ")[45].split(",")
+    return [i.strip() for i in l]
+
